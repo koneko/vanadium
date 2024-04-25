14c86e46117414526a374bf00798a29ae9761f94
koneko <67551503+koneko@users.noreply.github.com>
2024-04-25 19:46:01 +0200

    fixed oopsie

--- tables.sql
+++ tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE User (
-    ID      INT  PRIMARY KEY,
+    ID      INT  PRIMARY KEY AUTOINCREMENT,
     Name    TEXT NOT NULL UNIQUE,
     Token   TEXT NOT NULL,
     IsAdmin BOOL NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE User (
 );
 
 CREATE TABLE Cache (
-    ID         INTEGER PRIMARY KEY,
+    ID         INTEGER PRIMARY KEY AUTOINCREMENT,
     AnimeID    TEXT    NOT NULL,
     Title      TEXT    NOT NULL,
     Image      TEXT    NOT NULL,
@@ -18,16 +18,19 @@ CREATE TABLE Cache (
 );
 
 CREATE TABLE CacheLastUpdated (
-    ID   INT PRIMARY KEY UNIQUE,
+    ID   INT PRIMARY KEY UNIQUE AUTOINCREMENT,
     Time INT NOT NULL
 );
 
-CREATE TABLE User (
-    ID      INT  PRIMARY KEY,
-    Name    TEXT NOT NULL UNIQUE,
-    Token   TEXT NOT NULL,
-    IsAdmin BOOL NOT NULL,
-    Hash    TEXT NOT NULL,
-    Anime   TEXT NOT NULL,
-    History TEXT NOT NULL
+CREATE TABLE Anime (
+    AnimeID     TEXT    NOT NULL PRIMARY KEY,
+    Title       TEXT    NOT NULL,
+    Aliases     TEXT    NOT NULL,
+    Image       TEXT    NOT NULL,
+    Description TEXT    NOT NULL,
+    Episodes    INTEGER NOT NULL,
+    Date        TEXT    NOT NULL,
+    Genres      TEXT    NOT NULL,
+    LastUpdated INT     NOT NULL
 );
+
